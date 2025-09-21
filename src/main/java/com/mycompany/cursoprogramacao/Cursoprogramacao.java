8aa793782aeab95b1740df1bf18f2ed7060ea709
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-21 10:25:59 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/cursoprogramacao/Cursoprogramacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cursoprogramacao;
+
+/**
+ *
+ * @author Sousa
+ */
+public class Cursoprogramacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
