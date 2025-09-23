c2377defe7a32daa27db599ec29402e9d49e94ea
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-23 15:51:28 +0100

    feat: update funcoes matematicas em java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,23 @@
 
 public class Main {
     public static void main(String[] args){
+        double a = 4.0;
+        double b = 9.0;
+        double c = -4.0;
+        double delta;
+        double x1;
+        double x2;
         
+        delta = Math.pow(b, 2.0) - 4 * a * c;
+        
+        x1 = (- b + Math.sqrt(delta)) / (2.0 * a);
+        x2 = (- b - Math.sqrt(delta)) / (2.0 * a);
+        System.out.printf("%.2f%n", x1);
+        System.out.printf("%.2f%n", x2);
+        
+        
+        /*
+        //  EXERCICIO DE FUNCOES MATEMATICAS 1
         double x = 3.0;
         double y = 4.0;
         double z = -5.0;
@@ -26,7 +42,7 @@ public class Main {
         A = Math.abs(y);
         B = Math.abs(z);
         System.out.println("Valor absoluto de " + y + " = " + A);
-        System.out.println("Valor absoluto de " + z + " = " + B);
+        System.out.println("Valor absoluto de " + z + " = " + B);*/
         
         /*
         Locale.setDefault(Locale.US);
