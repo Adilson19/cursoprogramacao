93a17ff840c4dc56a1c5b61b650896abe797ed8d
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-23 15:11:33 +0100

    feat: funcoes matematicas em java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,36 @@
-import java.util.Locale;
-import java.util.Scanner;
+//import java.util.Locale;
+//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double A, B, C;
+        
+        A = Math.sqrt(x);
+        B = Math.sqrt(y);
+        C = Math.sqrt(25.0);
+        System.out.println("Raiz quadrada de " + x + " = " + A);
+        System.out.println("Raiz quadrada de " + y + " = " + B);
+        System.out.println("Raiz quadrada de 25 = " + C);
+        
+        A = Math.pow(x, y);
+        B = Math.pow(x, 2.0);
+        C = Math.pow(5.0, 2.0);
+        System.out.println(x + " elevado a " + y + " = " + A);
+        System.out.println(x + " elevado ao quadrado = " + B);
+        System.out.println("5 elevado ao quadrado = " + C);
+        
+        A = Math.abs(y);
+        B = Math.abs(z);
+        System.out.println("Valor absoluto de " + y + " = " + A);
+        System.out.println("Valor absoluto de " + z + " = " + B);
+        
+        /*
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
         
         int x;
         String s1, s2, s3;
@@ -21,7 +46,7 @@ public class Main {
         System.out.println(x);
         System.out.println(s1);
         System.out.println(s2);
-        System.out.println(s3);
+        System.out.println(s3);*/
         
         
         
@@ -65,7 +90,7 @@ public class Main {
         //  Verificando se realmente funcionou
         //System.out.println("Voce digitou: " + x);
         
-        sc.close();
+        //sc.close();
         
         /*
         int y = 32;
