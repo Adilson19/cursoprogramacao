dfe4f10ca7fafd24df64004c36f0876583d1b294
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-26 14:03:40 +0100

    feat: implementando os conceitos de toString no codigo

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -18,8 +18,10 @@ public class Program {
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
         
-        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+        //product.toString();
         
+        //System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+        System.out.println(product);
         sc.close();
     }
 }
