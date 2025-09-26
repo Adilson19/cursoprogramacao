d9e5df897c3cc92dbd4fbc5e6c58874c5b4a94af
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-26 14:15:32 +0100

    feat: finalizando o exercicio

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -19,9 +19,28 @@ public class Program {
         product.quantity = sc.nextInt();
         
         //product.toString();
-        
         //System.out.println(product.name + ", " + product.price + ", " + product.quantity);
-        System.out.println(product);
+        
+        System.out.println();
+        System.out.println("Product data: " + product);
+        
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+        
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        
+        System.out.println();
+        System.out.print("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        
+        
         sc.close();
     }
 }
