36b1da3fed853973a62966474f5ab280e07475a7
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-28 13:20:49 +0100

    feat: METODOS ESTATICOS: part. 1

---
+++ src/main/java/application/ProgramCalculator.java
@@ -0,0 +1,21 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+public class ProgramCalculator {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Entre radius: ");
+        double radius = sc.nextDouble();
+        
+        double c = circunference(radius);
+        
+        double v = volume(radius);
+        
+        System.out.println("Circunference: %.2f%n", c);
+        System.out.println("Volume: %.2f%n", );
+        
+        sc.close();
+    }
+}
