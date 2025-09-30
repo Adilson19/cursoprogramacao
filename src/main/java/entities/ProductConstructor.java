56a4da207f39f9338604afdbf93ebfbc4da9f1ea
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-30 10:43:43 +0100

    feat: update CONSTRUCTOR

--- src/main/java/entities/ProductConstructor.java
+++ src/main/java/entities/ProductConstructor.java
@@ -36,4 +36,14 @@ public class ProductConstructor {
     public void removeProduct(int quantity){
         this.quantity -= quantity;
     }
+    
+    public String toString(){
+        return name
+                + ", $"
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
 }
