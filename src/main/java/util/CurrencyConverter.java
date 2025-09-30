144e6d537775dd44c78e0b889938c253472cb220
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-30 08:43:14 +0100

    feat: MEMBROS ESTATICOS EXERCICIO FIXACAO RESOLVIDO

--- src/main/java/util/CurrencyConverter.java
+++ src/main/java/util/CurrencyConverter.java
@@ -1,9 +1,9 @@
 package util;
-public class CurrencyConverter {
-    public static final double IOF = 0.06;
-    
-    
-    
 
-    
+public class CurrencyConverter {
+	private static final double IOF = 6.00;
+
+	public static double getPaid(double exchange, double bought) {
+		return (bought * exchange) * (1 + IOF / 100);
+	}
 }
