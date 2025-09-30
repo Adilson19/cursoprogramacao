c2e1417bdc3f4e034fbb54660b1c21095d491328
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-30 09:00:44 +0100

    feat: MEMBROS ESTATICOS EXERCICIO FIXACAO RESOLVIDO update

--- src/main/java/application/ProgramFixacao.java
+++ src/main/java/application/ProgramFixacao.java
@@ -8,13 +8,13 @@ import util.CurrencyConverter;
 public class ProgramFixacao {
 
 	public static void main(String[] args) {
-		/*
-		 * Faça um programa para ler a cotação do dólar, e depois um valor em dólares a
-		 * ser comprado por uma pessoa em reais. Informar quantos reais a pessoa vai
-		 * pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF
-		 * sobre o valor em dólar. Criar uma classe 'CurrencyConverter' para ser
-		 * responsável pelos cálculos.
-		 */
+/*
+ * Faça um programa para ler a cotação do dólar, e depois um valor em dólares a
+ * ser comprado por uma pessoa em reais. Informar quantos reais a pessoa vai
+ * pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF
+ * sobre o valor em dólar. Criar uma classe 'CurrencyConverter' para ser
+ * responsável pelos cálculos.
+ */
 		Locale.setDefault(Locale.US);
 
 		Scanner sc = new Scanner(System.in);
--- src/main/java/util/CurrencyConverter.java
+++ src/main/java/util/CurrencyConverter.java
@@ -1,5 +1,13 @@
 package util;
 
+/*
+* Faça um programa para ler a cotação do dólar, e depois um valor em dólares a
+* ser comprado por uma pessoa em reais. Informar quantos reais a pessoa vai
+* pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF
+* sobre o valor em dólar. Criar uma classe 'CurrencyConverter' para ser
+* responsável pelos cálculos.
+*/
+
 public class CurrencyConverter {
 	private static final double IOF = 6.00;
 
