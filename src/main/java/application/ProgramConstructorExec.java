c174b08775a41aa30f0d291dd03c2fb7e699d739
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-30 09:52:49 +0100

    feat: CONSTRUTORES

---
+++ src/main/java/application/ProgramConstructorExec.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Scanner;
+import java.util.Locale;
+import entities.ProductConstructor;
+
+public class ProgramConstructorExec {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        // Recebe uma variavel temporaria
+        String name = sc.nextLine();
+        System.out.println("Price: ");
+        //  Recebe uma variavel temporaria
+        double price =  sc.nextDouble();
+        System.out.println("Quantity in stock: ");
+        //  Recebe uma variavel temporaria
+        int quantity = sc.nextInt();
+        //  Apos preencher todas variaveis so assim podemos instanciar O Objeto
+        ProductConstructor product = new ProductConstructor(name, price, quantity);
+        
+        System.out.println();
+        System.out.println("Product data: "+ product);
+        
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+        
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        
+        System.out.println();
+        System.out.println("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProduct(quantity);
+        
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        
+        sc.close();
+    }
+}
