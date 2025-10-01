ff91c1eae9e3bead67ce939b41416e120ed9c76c
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-01 09:42:02 +0100

    feat: putting classes and constructors

--- src/main/java/entities/Banco.java
+++ src/main/java/entities/Banco.java
@@ -1,12 +1,25 @@
 package entities;
 public class Banco {
-    protected int accountNumber;
-    public String name;
-    protected double initialValue;
-    protected double depositValue;
-    protected double sakeValue;
-    protected double accountBalance;
+    private int accountNumber;
+    private String name;
+    private char response;
+    private double initialValue;
+    private double depositValue;
+    private double sakeValue;
+    private double accountBalance;
     
+    //  Constructor
+    public Banco(int accountNumber, String name, double initialValue){
+        this.accountBalance = accountNumber;
+        this.name = name;
+        if(response == 'y'){
+            this.initialValue = initialValue;
+        }else{
+            this.initialValue = 0;
+        }
+    }
+    
+    //  Setando e Getando as variaveis
     public void setName(String name){
         this.name = name;
     }
@@ -18,6 +31,20 @@ public class Banco {
         return accountNumber;
     }
     
+    public void setInitialValue(double initialValue){
+        this.initialValue = initialValue;
+    }
+    
+    public void setDepositValue(double depositValue){
+        this.depositValue = depositValue;
+    }
+    
+    public double getAccountBalance(){
+        return accountBalance;
+    }
+    
+    
+    //  Metodos usados pelas classes
     public void deposit(){
         depositValue += initialValue;
     }
