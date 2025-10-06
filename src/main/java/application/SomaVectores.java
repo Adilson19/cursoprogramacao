ee4e837040e80293f9399e8911dce5d1e06a6dad
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-06 22:21:34 +0100

    feat: SOMA VECTORES RESOLVIDO

--- src/main/java/application/SomaVectores.java
+++ src/main/java/application/SomaVectores.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package application;
+import java.util.Scanner;
+import java.util.Locale;
 
-/**
- *
- * @author Sousa
- */
 public class SomaVectores {
-    
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Quantos valores vai ter cada vector? ");
+        int n = sc.nextInt();
+        int[] vectSoma1 = new int[n];
+        System.out.println("Digite os valores do vetor A:");
+        for(int i = 0; i < vectSoma1.length; i++){
+            vectSoma1[i] = sc.nextInt();
+        }
+        
+        int[] vectSoma2 = new int[n];
+        System.out.println("Digite os valores do vetor B:");
+        for(int i = 0; i < vectSoma2.length; i++){
+            vectSoma2[i] = sc.nextInt();
+        }
+        
+        int[] vectTotal = new int[n];
+        System.out.println("VETOR RESULTANTE:");
+        for(int i = 0; i < vectTotal.length; i++){
+            vectTotal[i] = vectSoma1[i] + vectSoma2[i];
+            System.out.println(vectTotal[i]);
+        }
+        
+        
+        sc.close();
+    }
 }
