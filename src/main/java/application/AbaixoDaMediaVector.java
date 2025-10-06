3d2d06856fd16229950f807dfd49b488c63fdb4e
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-06 22:57:39 +0100

    feat: update solving

--- src/main/java/application/AbaixoDaMediaVector.java
+++ src/main/java/application/AbaixoDaMediaVector.java
@@ -8,20 +8,24 @@ public class AbaixoDaMediaVector {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
         
-        System.out.println("Quantos elementos vai ter o vetor? ");
+        System.out.print("Quantos elementos vai ter o vetor? ");
         int n = sc.nextInt();
+        sc.nextLine();
         
         double[] vect = new double[n];
         double soma = 0.0;
         for(int i = 0; i < vect.length; i++){
+            System.out.printf("Digite um numero: ", vect[i]);
+            sc.nextLine();
+            vect[i] = sc.nextDouble();
             soma += vect[i];
         }
         double media = soma / vect.length;
-        System.out.printf("MEDIA DO VETOR = %.1f", media);
+        System.out.printf("MEDIA DO VETOR = %.3f", media);
         System.out.println("ELEMENTOS ABAIXO DA MEDIA: ");
         for(int i = 0; i < vect.length; i++){
             if(vect[i]< media){
-                
+                System.out.printf("%.1f%n", vect[i]);
             }
         }
         
