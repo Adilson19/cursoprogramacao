2fcdeb0fcecec40f6d128dd46a4262d0446e1cff
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-06 15:58:34 +0100

    feat: Negativos Vector Resolvido

--- src/main/java/application/NegativosVector.java
+++ src/main/java/application/NegativosVector.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package application;
-
-/**
- *
- * @author Sousa
- */
+import java.util.Scanner;
+import java.util.Locale;
 public class NegativosVector {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Quantos numeros voce vai digitar? ");
+        int n  = sc.nextInt();
+        int[] vect = new int [n];
+        for(int i = 0; i < vect.length; i++){
+            System.out.println("Digite um numero: ");
+            vect[i] = sc.nextInt();
+        }
+        System.out.println("NUMEROS NEGATIVOS: ");
+        for(int i = 0; i < vect.length; i++){
+            if(vect[i] < 0){
+                System.out.println(vect[i]);
+            }
+        }
+    }
     
 }
