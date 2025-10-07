afc5f3c21800c1bc77156ffa4e4e1aa50678aecc
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-07 10:24:42 +0100

    feat: DEMO WRAPPER CLASSES

---
+++ src/main/java/entities/Demo.java
@@ -0,0 +1,14 @@
+package entities;
+public class Demo {
+    // Boxing and Unboxing
+    Integer x = 10;
+    int y = x * 2;
+    
+    //  Wrapper Classes
+    public class DemoWrapper{
+        public String name;
+        public Double price;
+        public Integer quantity;
+    }
+}
+
