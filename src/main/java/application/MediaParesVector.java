fb11eb3b33b4e9ea0c057cf136c4858c08d4787a
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-07 11:07:17 +0100

    feat: MEDIA PARES VECTOR RESOLVIDO

--- src/main/java/application/MediaParesVector.java
+++ src/main/java/application/MediaParesVector.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package application;
-
-/**
- *
- * @author Sousa
- */
+import java.util.Scanner;
+import java.util.Locale;
 public class MediaParesVector {
+    public static void main(String[] args){
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
     
+    System.out.print("Quantos elementos vai ter o vetor? ");
+    int n = sc.nextInt();
+    int[] vect = new int [n];
+    int soma = 0;
+    int cont = 0;
+    for(int i = 0; i< vect.length; i++){
+        System.out.print("Digite um numero: ");
+        vect[i] = sc.nextInt();
+        if(vect[i]%2==0){
+            soma += vect[i];
+            cont++;
+        }        
+    }
+    double media = 0.0;
+    if(soma > 0){
+        media = soma / cont;
+        System.out.printf("MEDIA DOS PARES = %.1f", media);
+    }else if(soma==0){
+        System.out.println("NENHUM NUMERO PAR");
+    }
+    
+    sc.close();
+    }
 }
