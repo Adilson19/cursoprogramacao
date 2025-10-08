2e876729e23ba6c8a5bb48c47c5a4d34b97b5ced
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-08 09:27:05 +0100

    feat: Criando a estrutura do exercicio

---
+++ src/main/java/entities/EmployeeList.java
@@ -0,0 +1,31 @@
+package entities;
+public class EmployeeList {
+    private Integer id;
+    private Character name;
+    private Double salary;
+    
+    public EmployeeList(Integer id, Character name, Double salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+    
+    public void setId(Integer id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+    
+    public void setName(Character name){
+        this.name = name;
+    }
+    public Character getName(){
+        return name;
+    }
+    
+    public Double getSalary(){
+        return salary;
+    }
+    
+}
