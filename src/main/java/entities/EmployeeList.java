778d396be4a28b0339f4fa055b7bee0549e01022
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-08 10:23:15 +0100

    feat: implementando a solução do Nélio Alves

--- src/main/java/entities/EmployeeList.java
+++ src/main/java/entities/EmployeeList.java
@@ -1,38 +1,49 @@
 package entities;
+
 public class EmployeeList {
-    private Integer id;
-    private Character name;
-    private Double salary;
-    
-    public EmployeeList(Integer id, Character name, Double salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
-    
-    public void setId(Integer id){
-        this.id = id;
-    }
-    public int getId(){
-        return id;
-    }
-    
-    public void setName(Character name){
-        this.name = name;
-    }
-    public Character getName(){
-        return name;
-    }
-    
-    public Double getSalary(){
-        return salary;
-    }
-    
-    public void searchingSalaryForIncrease(Integer id){
-        this.id = id;
-    }
-    public void setPercentage(Double percent){
-        this.salary += percent * 10 / 100;
-    }
-    
+
+	private Integer id;
+	private String name;
+	private Double salary;
+	
+	public EmployeeList() {
+	}
+	
+	public EmployeeList(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	public void increaseSalary(double percentage) {
+		salary += salary * percentage / 100.0;
+	}
+	
+	public String toString() {
+		return id + ", " + name + ", " + String.format("%.2f", salary);
+	}
 }
