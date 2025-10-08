0151c4d1471ade4d4b6653e76a382c9a90645fcc
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-08 10:42:13 +0100

    feat: Update solving list exercise

--- src/main/java/application/ListExerciseExc.java
+++ src/main/java/application/ListExerciseExc.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Employee;
 import entities.EmployeeList;
 
 public class ListExerciseExc {
@@ -15,7 +14,7 @@ public class ListExerciseExc {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		List<Employee> list = new ArrayList<>();
+		List<EmployeeList> list = new ArrayList<>();
 		
 		// PART 1 - READING DATA:
 		
@@ -60,15 +59,15 @@ public class ListExerciseExc {
 		
 		System.out.println();
 		System.out.println("List of employees:");
-		for (Employee obj : list) {
+		for (EmployeeList obj : list) {
 			System.out.println(obj);
 		}
 				
 		sc.close(); 
 	}
 	
-	public static boolean hasId(List<Employee> list, int id) {
-		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+	public static boolean hasId(List<EmployeeList> list, int id) {
+		EmployeeList emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
 		return emp != null;
 	}
 }
