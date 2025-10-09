fda869a07245482148041895d87673a8f9878fd9
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-09 21:46:26 +0100

    feat: update exercicio vector005 resolvido

--- src/main/java/exerciciosSobreVetoresMatrizes/Vector005.java
+++ src/main/java/exerciciosSobreVetoresMatrizes/Vector005.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exerciciosSobreVetoresMatrizes;
 
-/**
- *
- * @author Sousa
- */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Vector005 {
-    
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int n = 10;
+        int[] vectA = new int[n];
+        int[] vectB = new int[n];
+        
+        for(int i = 1; i < n; i++){
+            vectA[i] = sc.nextInt();
+            vectB[i] = vectA[i] * i;
+        }
+        
+        for(int i = 1; i < n; i++){
+            System.out.printf("VectA = %d, VectB = %d%n", vectA[i], vectB[i]);
+        }
+        
+        sc.close();
+    }
 }
