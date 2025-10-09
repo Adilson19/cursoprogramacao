340af32a4e7be0247501cd68cd2cbd0884f84a3c
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-09 21:24:50 +0100

    feat: update exercicio vector003 resolvido

--- src/main/java/exerciciosSobreVetoresMatrizes/Vector003.java
+++ src/main/java/exerciciosSobreVetoresMatrizes/Vector003.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exerciciosSobreVetoresMatrizes;
 
-/**
- *
- * @author Sousa
- */
+import static java.lang.Math.pow;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Vector003 {
-    
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc =  new Scanner(System.in);
+        
+        
+        int n = 15;
+        int[] vectA = new int [n];
+        int[] vectB = new int [n];
+        
+        for(int i = 0; i < vectA.length; i++){
+            vectA[i] = sc.nextInt();
+            vectB[i] = vectA[i];
+            vectB[i] = (int) pow(vectB[i], 2);
+        }
+        
+        for(int i = 0; i < vectA.length; i++){
+            System.out.printf("VectA = %d, VectB = %d%n", vectA[i], vectB[i]);
+        }
+        
+        sc.close();
+    }
 }
