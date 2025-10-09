088606e422dcb59c8c5e9c33e1020b804cf60454
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-09 21:14:38 +0100

    feat: update vector002 Resolvido

--- src/main/java/exerciciosSobreVetoresMatrizes/Vector002.java
+++ src/main/java/exerciciosSobreVetoresMatrizes/Vector002.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exerciciosSobreVetoresMatrizes;
+import java.util.Scanner;
+import java.util.Locale;
 
-/**
- *
- * @author Sousa
- */
 public class Vector002 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int n = 8;
+        int[] vectA = new int [n];
+        int[] vectB = new int [n];
+        
+        for(int i = 0; i < vectA.length; i++){
+            vectA[i] = sc.nextInt();
+            vectB[i] = vectA[i];
+            vectB[i] = vectB[i] * 2;
+        }
+        
+        for(int i = 0; i < vectA.length; i++){
+            System.out.printf("VectA = %d, VectB = %d%n", vectA[i], vectB[i]);
+        }
+        
+        sc.close();
+    }
     
 }
