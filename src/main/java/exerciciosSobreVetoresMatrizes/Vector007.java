6fa99dfe133759fd257cfbcaaa850a8de4a91db9
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-09 22:09:52 +0100

    feat: exercicio vector007 resolvido

--- src/main/java/exerciciosSobreVetoresMatrizes/Vector007.java
+++ src/main/java/exerciciosSobreVetoresMatrizes/Vector007.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exerciciosSobreVetoresMatrizes;
 
-/**
- *
- * @author Sousa
- */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Vector007 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int n =10;
+        int[] vectA = new int[n];
+        int[] vectB = new int[n];
+        int[] vectC = new int[n];
+        
+        for(int i = 0; i < vectA.length; i++){
+            vectA[i] = sc.nextInt();
+            sc.nextLine();
+            vectB[i] = sc.nextInt();
+            sc.nextLine();
+            vectC[i] = vectA[i] - vectB[i];
+        }
+        
+        for(int i = 0; i < vectA.length; i++){
+            System.out.printf("VectA: %d - VectB: %d = VectC: %d%n", vectA[i], vectB[i], vectC[i]);
+        }
+        
+        sc.close();
+    }
     
 }
