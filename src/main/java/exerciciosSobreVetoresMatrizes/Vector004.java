5c0f8bab19a070273941719b068d539837d732cd
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-09 21:34:22 +0100

    feat: exercicio de vector004 resolvido

--- src/main/java/exerciciosSobreVetoresMatrizes/Vector004.java
+++ src/main/java/exerciciosSobreVetoresMatrizes/Vector004.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exerciciosSobreVetoresMatrizes;
 
-/**
- *
- * @author Sousa
- */
+import static java.lang.Math.sqrt;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Vector004 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int n = 15;
+        int[] vectA = new int[n];
+        double[] vectB = new double[n];
+        
+        for(int i = 0; i < vectA.length; i++){
+            vectA[i] = sc.nextInt();
+            vectB[i] = vectA[i];
+            vectB[i] = sqrt(vectB[i]);
+        }
+        
+        for(int i = 0; i < vectA.length; i++){
+            System.out.printf("VectA = %d, VectB = %.2f%n", vectA[i], vectB[i]);
+        }
+        
+        sc.close();
+    }
     
 }
