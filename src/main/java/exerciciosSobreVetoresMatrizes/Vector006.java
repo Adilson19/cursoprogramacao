7c5ebe0574e3d1f9f65b51d5ba07adbb5cb3efec
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-09 21:57:39 +0100

    feat: update exercicio Vect006 resolvido

--- src/main/java/exerciciosSobreVetoresMatrizes/Vector006.java
+++ src/main/java/exerciciosSobreVetoresMatrizes/Vector006.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package exerciciosSobreVetoresMatrizes;
 
-/**
- *
- * @author Sousa
- */
+package exerciciosSobreVetoresMatrizes;
+import java.util.Scanner;
+import java.util.Locale;
 public class Vector006 {
-    
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int n = 10;
+        int[] vectA = new int[n];
+        int[] vectB = new int[n];
+        int[] vectC = new int[n];
+        
+        for(int i = 0; i < vectA.length; i++){
+            vectA[i] = sc.nextInt();
+            vectB[i] = sc.nextInt();
+            vectC[i] = vectA[i] + vectB[i];
+        }
+        
+        for(int i = 0; i < vectA.length; i++){
+            System.out.printf("VectA: %d + VectB: %d = VectC: %d%n", vectA[i], vectB[i], vectC[i]);
+        }
+        
+        sc.close();
+    }
 }
