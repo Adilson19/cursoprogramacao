8f2271e8b1741c46ce517e998806ae5d292a5c3a
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-11 22:36:10 +0100

    feat: update solution of exercise

--- src/main/java/exerciciosSobreVetoresMatrizes/InteirosMaisLongo.java
+++ src/main/java/exerciciosSobreVetoresMatrizes/InteirosMaisLongo.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exerciciosSobreVetoresMatrizes;
 
-/**
- *
- * @author Sousa
- */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class InteirosMaisLongo {
+    public static final int TAMANHO_MAXIMO_ARRAY = 10;
+    public static void main(String[] args){        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        
     
+    
+    
+    
+        sc.close();
+    }
 }
