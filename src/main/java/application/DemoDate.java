9654874666565cdb6da6ac24f0c007968f15deeb
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-12 08:04:28 +0100

    feat: DataTimes part1

--- src/main/java/application/DemoDate.java
+++ src/main/java/application/DemoDate.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package application;
 
-/**
- *
- * @author Sousa
- */
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class DemoDate {
-    
+    public static void main(String[] args){
+        //  Retorna a data do Sistema e instanciou no Objeto
+        LocalDate d01 = LocalDate.now();
+        //  Data hora local
+        LocalDateTime d02 = LocalDateTime.now();
+        //  Horario Global - De Londes, GMT
+        Instant d03 = Instant.now();
+        //
+        LocalDate d04 = LocalDate.parse("2025-10-12");
+        //  
+        LocalDateTime d05 = LocalDateTime.parse("2025-10-12T08:02:30");
+        
+        System.out.println("d01 = " + d01);
+        System.out.println("d02 = " + d02);
+        System.out.println("d03 = " + d03);
+        System.out.println("d04 = " + d04);
+        System.out.println("d05 = " + d05);
+        
+        
+        
+    }    
 }
