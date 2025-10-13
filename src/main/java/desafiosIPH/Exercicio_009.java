59ce2d433e3a9a71a877539b31e5a8b3fea1cbea
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-13 21:02:40 +0100

    feat: Desafio IPH 009 - Resolvido

--- src/main/java/desafiosIPH/Exercicio_009.java
+++ src/main/java/desafiosIPH/Exercicio_009.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package desafiosIPH;
 
-/**
- *
- * @author Sousa
- */
+import java.util.Scanner;
+
 public class Exercicio_009 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        final double SALARIO_FIXO = 500.00;
+        final double BONUS_POR_VENDA = 50.00;
+        double bonusTotal;
+        double salarioTotal = 0.0;
+        int softwareVendidos =0;
+        
+        System.out.println("Digite a quantidade de Softwares que o funcionario vendeu: ");
+        softwareVendidos = sc.nextInt();
+        bonusTotal = softwareVendidos * BONUS_POR_VENDA;
+        salarioTotal = SALARIO_FIXO + bonusTotal;
+        System.out.println("O Salario total do Funcionario = "+salarioTotal);
+        
+        sc.close();
+    }
 }
