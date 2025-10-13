cfe1c0a6cb5272a7c94755766d77fec6452c73d1
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-13 13:30:00 +0100

    feat: Desafio IPH 007 - Resolvido

--- src/main/java/desafiosIPH/Exercicio_007.java
+++ src/main/java/desafiosIPH/Exercicio_007.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package desafiosIPH;
 
-/**
- *
- * @author Sousa
- */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio_007 {
-    
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        double saldoCliente = 500.00;
+        double saldoAdicional = 0;
+        
+        System.out.print("Digite o valor do Cheque: ");
+        saldoAdicional = sc.nextDouble();
+        saldoCliente += saldoAdicional;
+        System.out.printf("O saldo atual do cliente = %.2f AKZ%n", saldoCliente);
+        
+        
+        sc.close();
+    }
 }
