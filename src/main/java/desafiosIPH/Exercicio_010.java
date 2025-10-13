c7f28f7240b703af984649b584d3de405e597782
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-13 23:20:08 +0100

    feat: Desafio IPH 010 - Resolvido

--- src/main/java/desafiosIPH/Exercicio_010.java
+++ src/main/java/desafiosIPH/Exercicio_010.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package desafiosIPH;
+import java.util.Scanner;
 
-/**
- *
- * @author Sousa
- */
 public class Exercicio_010 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        double salarioLiquido = 0.0;
+        double salarioBruto = 0.0;
+        final double PERCENTAGEM = 0.09;
+        double desconto;
+        String nomeDoFuncionario;
+        
+        System.out.println("Digite o nome do Funcionario:");
+        nomeDoFuncionario = sc.nextLine();
+        System.out.println("Digite o Salario Bruto:");
+        salarioBruto = sc.nextDouble();
+        desconto = salarioBruto * PERCENTAGEM;
+        salarioLiquido = salarioBruto - desconto;
+        System.out.printf("Sr.(a) %s, o seu Salario Liquido %.2f que sofreu um Desconto de %.2f AKZ%n", nomeDoFuncionario, salarioLiquido, desconto);
+        sc.close();
+    }
 }
