19adf636dd4188e1501b6b64053227fd38825461
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-14 20:08:03 +0100

    feat: Desafio IPH 012 - Resolvido

--- src/main/java/desafiosIPH/Exercicio_013.java
+++ src/main/java/desafiosIPH/Exercicio_013.java
@@ -1,9 +1,28 @@
 package desafiosIPH;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio_013 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		System.out.println("Digite um numero: ");
+		a = sc.nextInt();
+		System.out.println("Digite um outro numero: ");
+		b = sc.nextInt();
+		if(a > b) {
+			System.out.printf("%d - %d%n", b, a);
+		}else if(a < b) {
+			System.out.printf("%d - %d%n", a, b);
+		}else {
+			System.out.println("Nao podem ser numeros iguais!");
+		}
+		
+		sc.close();
 
 	}
 
