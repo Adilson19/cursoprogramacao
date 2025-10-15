607ef10b8182bc5753b1bf9af3214ae7e574bf29
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-15 08:47:33 +0100

    feat: Studying DateTime

--- src/main/java/application/application/DemoDateTime.java
+++ src/main/java/application/application/DemoDateTime.java
@@ -3,9 +3,14 @@ package application;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DemoDate {
     public static void main(String[] args){
+    	
+    	DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    	DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    	
         //  Retorna a data do Sistema e instanciou no Objeto
         LocalDate d01 = LocalDate.now();
         //  Data hora local
@@ -18,13 +23,29 @@ public class DemoDate {
         LocalDateTime d05 = LocalDateTime.parse("2025-10-12T08:02:30");
         //	Gerar uma data-hora no padrao UTC
         Instant d06 = Instant.parse("2025-10-15T06:50:25Z");
+        //	
+        Instant d07 = Instant.parse("2025-10-15T06:50:25");
         
+        //	Customizado
+        LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
+        //	Outra maneira de customizar a data a partir do DateTimeFormatter
+        LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+        //	
+        LocalDate d10 = LocalDate.of(2022, 7, 20);
+        //	
+        LocalDateTime d11 = LocalDateTime.of(2022, 7, 20, 1, 30);
+        //	
         System.out.println("d01 = " + d01);
         System.out.println("d02 = " + d02);
         System.out.println("d03 = " + d03);
         System.out.println("d04 = " + d04);
         System.out.println("d05 = " + d05);
         System.out.println("d06 = " + d06);
+        System.out.println("d07 = " + d07);
+        System.out.println("d08 = " + d08);
+        System.out.println("d09 = " + d09);
+        System.out.println("d010 = " + d10);
+        System.out.println("d011 = " + d11);
         
         
     }    
