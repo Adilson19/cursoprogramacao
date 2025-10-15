710c4a104c1e57c36422d822f3c569fef058585c
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-15 09:35:27 +0100

    feat: Studying DateTime part2

--- src/main/java/application/DemoDate.java
+++ src/main/java/application/DemoDate.java
@@ -3,9 +3,53 @@ package application;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class DemoDate {
     public static void main(String[] args){
+    	LocalDate d04 = LocalDateTime.parse("2022-07-20");
+    	LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+    	Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+    	
+    	DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    	DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    	DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+    	
+    	System.out.println("d04 = " + d04.format(fmt1));
+    	System.out.println("d04 = " + fmt1.format(d04));
+    	System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+    	
+    	//	Formatando o LocalDateTime com hora e minuto
+    	System.out.println("d05 = " + d05.format(fmt1));
+    	System.out.println("d05 = " + d05.format(fmt2));
+    	
+    	System.out.println("d06 = " + fmt3.format(d06));
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	/*
         //  Retorna a data do Sistema e instanciou no Objeto
         LocalDate d01 = LocalDate.now();
         //  Data hora local
@@ -24,7 +68,7 @@ public class DemoDate {
         System.out.println("d03 = " + d03);
         System.out.println("d04 = " + d04);
         System.out.println("d05 = " + d05);
-        System.out.println("d06 = " + d06);
+        System.out.println("d06 = " + d06);*/
         
         
     }    
