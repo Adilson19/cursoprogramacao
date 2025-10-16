aa875438f2b82cd34022b866c37e5fb4f9d40f43
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-16 16:44:49 +0100

    feat: update Class Jogo e Informacoes Estatisticas de uma Equipa Num Jogo

---
+++ LigaDeFutebolPortuguesa/src/entities/InformacoesEstatisticasDeUmaEquipaNumJogo.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class InformacoesEstatisticasDeUmaEquipaNumJogo {
+	private int numeroDeGolosMarcados;
+	private int numeroDeGolosSofridos;
+	private int numeroDePassesTotal;
+	private int numeroDePassesCorretos;
+}
---
+++ LigaDeFutebolPortuguesa/src/entities/Jogo.java
@@ -0,0 +1,8 @@
+package entities;
+import entities.Equipa;
+
+public class Jogo {
+	private Object Equipa;
+	private int resultadoFinal;
+	private int tempoDePosseDeBola;
+}
