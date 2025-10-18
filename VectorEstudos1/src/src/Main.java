2a417fcd62ae1ac298e2863984c46b6680db1241
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-18 22:58:00 +0100

    feat: Desafio incompleto

---
+++ VectorEstudos1/src/src/Main.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.util.Scanner;
+/*
+ * 3- Contagem de Pares e Ímpares: 
+ * Leia um vetor de 15 números inteiros. 
+ * Conte e imprima quantos números são pares e quantos são ímpares no vetor.
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = 15;
+		int[] vect = new int[n];
+		System.out.println("Digite 15 numeros inteiros: ");
+		for(int i = 0; i < n; i++) {
+			vect[i] = sc.nextInt();
+		}
+		int cont = 1;
+		for(int i = 0; i < n; i++) {
+			if(){
+				
+			}
+		}
+		
+		sc.close();
+	}
+}
