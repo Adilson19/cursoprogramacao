cb5a6bebcfe64bd2abf7859a03395b9c2689f2fa
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-20 20:15:46 +0100

    feat: Update

--- FusoHorario/src/src/Main.java
+++ FusoHorario/src/src/Main.java
@@ -1,9 +1,21 @@
 package src;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ZonedDateTime meuFuso = ZonedDateTime.now();
+		ZoneId saoPaulo = ZoneId.of("America/Sao_Paulo");
+		ZonedDateTime agoraSaoPaulo = meuFuso.withZoneSameInstant(saoPaulo);
+		ZoneId londres = ZoneId.of("Europe/London");
+		ZonedDateTime agoraLondres = meuFuso.withZoneSameInstant(londres);
+		
+		
+		System.out.println("Hora atual (Local): " + meuFuso);
+		System.out.println("Hora em São Paulo: " + agoraSaoPaulo);
+		System.out.println("Hora em Londres: " + agoraLondres);
 
 	}
 
