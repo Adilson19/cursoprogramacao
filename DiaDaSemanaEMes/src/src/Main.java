e2619277323ffa98582108d4373e5ab015ce0138
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-20 20:01:50 +0100

    feat: update

--- DiaDaSemanaEMes/src/src/Main.java
+++ DiaDaSemanaEMes/src/src/Main.java
@@ -1,10 +1,21 @@
 package src;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LocalDate feriado = LocalDate.of(2025, 11, 2);
+		DayOfWeek diaDaSemana = feriado.getDayOfWeek();
+		Month mes = feriado.getMonth();
+		boolean isFimDeSemana = diaDaSemana == DayOfWeek.SATURDAY || diaDaSemana == DayOfWeek.SUNDAY; 
+		
+		System.out.println("Data do feriado = " + feriado);
+		System.out.println("O dia da Semana = " + diaDaSemana);
+		System.out.println("O nome do mes = " + mes);
+		System.out.println("Cai em fim de semana? " + isFimDeSemana);
 	}
 
 }
