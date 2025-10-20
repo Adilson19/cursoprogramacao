4e263c528934e8cece35963c071fc7dcee45ec5b
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-20 19:47:08 +0100

    feat: update

--- DiferencaEntreDatas/src/src/Main.java
+++ DiferencaEntreDatas/src/src/Main.java
@@ -1,10 +1,26 @@
 package src;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LocalDate dataInicial = LocalDate.now();
+		LocalDate dataFinal = LocalDate.parse("2028-10-20");
+		
+		Period periodo = Period.between(dataInicial, dataFinal);
+		System.out.println("Diferenca entre as datas: " + periodo.getYears() + " anos, " + periodo.getMonths() + " meses e " + periodo.getDays() + " dias.");
+		
+		LocalTime horaInicial = LocalTime.parse("08:00:00");
+		LocalTime horaFinal = LocalTime.parse("15:30:45");
+		Duration duracao = Duration.between(horaInicial, horaFinal);
+		long horas = duracao.toHours();
+		long minutos = duracao.toMinutes() % 60;
+		long segundos = duracao.toSeconds() % 60;
+		System.out.println("Diferenca entre as datas: " + horas + "h " + minutos + "m " + segundos + "s");
 	}
 
 }
