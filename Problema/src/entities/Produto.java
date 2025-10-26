bbdd270746f409492d539c46c49bd227c6ce1f9f
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-26 20:33:16 +0100

    feat: update implementing class Produto and  calling in class Main

---
+++ Problema/src/entities/Produto.java
@@ -0,0 +1,54 @@
+package entities;
+
+public class Produto {
+	//	Atributos
+	private String name;
+	private Double price;
+	private Integer quantity;
+	
+	public Produto(String name, Double price, Integer quantity){
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	//	Getters e Setters
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	
+	
+	public void setPrice(Double price){
+		this.price = price;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	
+	//	Metodos
+	
+	public double valorTotalEmStoque(){
+		return 0;
+	}
+	
+	public void adicionarProduto(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removerProduto(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	
+	
+}
--- Problema/src/src/Main.java
+++ Problema/src/src/Main.java
@@ -1,9 +1,18 @@
 package src;
 
+import java.util.Scanner;
+import entities.Produto;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		sc.close();
 
 	}
 
