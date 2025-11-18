c45494ed4b75c620d69bff569a39a6a1df290360
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-11-18 08:58:17 +0100

    feat: update

--- Exemplo_TryCatch/src/application/Program.java
+++ Exemplo_TryCatch/src/application/Program.java
@@ -6,6 +6,19 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		method1();
+		System.out.println("End of program");
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+		
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
 		Scanner sc = new Scanner(System.in);
 		
 		try {
@@ -19,8 +32,8 @@ public class Program {
 		catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
-		System.out.println("End of program");
+		
 		sc.close();
+		System.out.println("***METHOD2 END***");
 	}
-
 }
