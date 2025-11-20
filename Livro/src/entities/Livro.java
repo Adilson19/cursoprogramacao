272eaf5a0fe103737583b6588af6304dbb0f7d52
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-11-20 20:19:16 +0100

    feat: building the final class

--- Livro/src/entities/Livro.java
+++ Livro/src/entities/Livro.java
@@ -8,5 +8,13 @@ public class Livro {
 	public Livro() {
 		
 	}
+	public Livro(String titulo, String autor, Integer numeroPaginas) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.numeroPaginas = numeroPaginas;
+	}
 	
+	public String mostrarDetalhes(String titulo, String autor, Integer numeroPaginas) {
+		return "O titulo: " + titulo + " Autor: " + autor + " Numero de Paginas: " + numeroPaginas;
+	}
 }
