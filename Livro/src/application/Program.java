c30c06926fb50606bee0c4a4898ad73cf5a00cf8
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-11-20 20:29:06 +0100

    feat: finnished

--- Livro/src/application/Program.java
+++ Livro/src/application/Program.java
@@ -1,10 +1,25 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.Livro;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);  
+		System.out.print("Digite o titulo de um livro: ");
+		String titulo = sc.nextLine();
+		System.out.print("Digite o nome do autor: ");
+		sc.next();
+		String autor = sc.nextLine();
+		System.out.print("Digite o numero de paginas do livro: ");
+		int numeroPaginas = sc.nextInt();
+		Livro livro = new Livro(titulo, autor, numeroPaginas);
+		System.out.print(livro.mostrarDetalhes(titulo, autor, numeroPaginas));
+		
+		
+		sc.close();
 	}
 
 }
--- Livro/src/entities/Livro.java
+++ Livro/src/entities/Livro.java
@@ -14,7 +14,25 @@ public class Livro {
 		this.numeroPaginas = numeroPaginas;
 	}
 	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Integer getNumeroPaginas() {
+		return numeroPaginas;
+	}
+	public void setNumeroPaginas(Integer numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
 	public String mostrarDetalhes(String titulo, String autor, Integer numeroPaginas) {
-		return "O titulo: " + titulo + " Autor: " + autor + " Numero de Paginas: " + numeroPaginas;
+		return "O titulo: " + titulo + " Autor: " + autor + " Numero de Paginas: " + numeroPaginas + "";
 	}
 }
