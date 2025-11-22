4d4e3d23b0cd069cdf68ebe2aba11fc14de1bf7a
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-11-22 22:15:51 +0100

    feat: trying solve some exercise

---
+++ DivisaoSegura/src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program {
+	public static void main(String[] args) {
+		 int numeroUm;
+		int numeroDois;
+		public void aceitaDoisNumeros(int numeroUm, int numeroDois) {
+			// Muitos bugs
+		}
+	}
+}
