9328f6f32f40fa619f737ce3f61cee8e6a8b2fd0
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-12-03 10:10:40 +0100

    feat: resolucao do primeiro desafio

--- DivisaoSegura/src/application/Program.java
+++ DivisaoSegura/src/application/Program.java
@@ -1,13 +1,24 @@
 package application;
 
 public class Program {
-	public static void main(String[] args) {
-		 int numeroUm;
-		int numeroDois;
-		
-		
-		public double aceitaDoisNumeros(int numeroUm, int numeroDois) {
-			// Muitos bugs
+	public static Double divisaoSegura(int numerador, int denominador) {
+		try {
+			//	Tenta realizar a divisao
+			//	Convertendo para double para permitir decimais
+			return (double) numerador / denominador;
+		}
+		catch(ArithmeticException e) {
+			//	Captura especificamente erros aritmetricos (como divisao por zero em inteiros)
+			System.out.println("Erro: Nao eh possivel dividir por zero.");
+			
+			//	Retorna -1 conforme sugerido no exercicio (ou poderia retornar null se o tipo fosse Double)
+			return -1.0;
 		}
 	}
+	 public static void main(String[] args) {
+		 //	Teste 1
+		 System.out.println("Resultado: " + divisaoSegura(10, 2));
+		 //	Teste 2
+		 System.out.println("Resultado: " + divisaoSegura(5, 0));
+	 }
 }
