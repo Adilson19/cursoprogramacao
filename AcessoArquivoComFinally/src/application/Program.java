58283d844555ac6b42b694dc4bf3b022c128c6a4
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-12-04 12:24:22 +0100

    feat: update

--- AcessoArquivoComFinally/src/application/Program.java
+++ AcessoArquivoComFinally/src/application/Program.java
@@ -7,6 +7,14 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("--- Execucao 1: Arquivo Nao Encontrado ---");
+		openningFile.tryingOpenFile("arquivo_inexistente.txt");
+		
+		System.out.println("\n"+"-".repeat(50)+"\n");
+		
+		System.out.println("--- Execucao 2: Sucesso (Assumindo que 'dados.txt' existe) ---");
+		openningFile.tryingOpenFile("dados.txt");
+		
 		sc.close();
 	}
 
