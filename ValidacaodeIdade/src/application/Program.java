1b22bcdc1ca10458b9b77f06122b722ca11972eb
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-12-04 08:31:39 +0100

    feat: update

--- ValidacaodeIdade/src/application/Program.java
+++ ValidacaodeIdade/src/application/Program.java
@@ -3,12 +3,7 @@ package application;
 import model.exceptions.idadeInvalidaError;
 
 public class Program {
-	public static void validarIdade(int idade) throws idadeInvalidaError{
-		if(idade < 0 || idade > 150) {
-			throw new idadeInvalidaError("A idade: " + idade + " nao eh permitida (limites: 0-150).");
-		}
-		System.out.println("Idade " + idade + " eh valido.");
-	}
+	
 	
 	public static void main(String[] args) {
 		try {			
